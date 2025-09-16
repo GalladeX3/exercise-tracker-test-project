ee9a15dfa1476e8e3c9bdf956053af34f8991a70
Waldo <waldito.07@gmail.com>
2025-09-16 17:24:40 -0400

    User.js - 1.0

--- models/User.js
+++ models/User.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, trim: true, unique: true }
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('User', userSchema);
 
